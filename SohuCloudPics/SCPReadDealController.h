c1f7c6c4f8736887afe915584b805107b6d31d97
Qu Ke <kequ@sohu-inc.com>
2013-01-13 10:43:18 +0800

    add readDealController

---
+++ SohuCloudPics/SCPReadDealController.h
@@ -0,0 +1,13 @@
+//
+//  SCPReadDealController.h
+//  SohuCloudPics
+//
+//  Created by sohu on 13-1-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCPReadDealController : UIViewController
+
+@end
